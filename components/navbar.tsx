7aaf0e5744fca700a472dbb0d969b64101e4d659
durk <103585220+durkisneer1@users.noreply.github.com>
2025-09-22 00:02:21 -0500

    Update branding and add constructors to docs
    Replaced the navbar logo and title with branding for Kraken Engine, updated the introduction page with a banner image, and added constructor signatures to all class documentation pages.

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -73,9 +73,9 @@ export function Navbar() {
 
 export function Logo() {
   return (
-    <Link href="/" className="flex items-center gap-2.5">
-      <CommandIcon className="w-6 h-6 text-muted-foreground" strokeWidth={2} />
-      <h2 className="text-md font-bold font-code">Kraken Docs</h2>
+    <Link href="/" className="flex items-center gap-2.5" id="kraken-banner">
+      <img src="/images/faviconnekked.png" />
+      <h2 className="text-md font-bold font-code">Kraken Engine</h2>
     </Link>
   );
 }
